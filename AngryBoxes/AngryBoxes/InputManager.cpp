3dedbc26ef36dcf217406811b6c235383897fb3f
unknown <1203172@MC10321.uad.ac.uk>
2014-10-21 15:01:10 +0100

    Set up basic input and set framework for launching bird

--- AngryBoxes/AngryBoxes/InputManager.cpp
+++ AngryBoxes/AngryBoxes/InputManager.cpp
@@ -1,1 +1,61 @@
 #include "InputManager.h"
+
+void InputManager::init(sf::Window* applicationPointer)
+{
+	windowPointer = applicationPointer;
+}
+
+//main input loop to process all inputs.
+void InputManager::getInput()
+{
+	while(windowPointer->pollEvent(EventManager))
+	{
+		switch(EventManager.type)
+		{
+			//Close window when requested
+			case sf::Event::Closed:
+			{
+				windowPointer->close();
+				break;
+			}
+
+			//Updates mouse position
+			case sf::Event::MouseMoved :
+			{
+				currentXPos = EventManager.mouseMove.x;
+				currentYPos = EventManager.mouseMove.y;
+				break;
+			}
+			
+			//Takes down drag start location when left click
+			case sf::Event::MouseButtonPressed :
+			{
+				if(EventManager.mouseButton.button == sf::Mouse::Left)
+				{
+					xDragStart = EventManager.mouseButton.x;
+					yDragStart = EventManager.mouseButton.y;
+				}
+
+				break;
+			}
+
+			
+			case sf::Event::MouseButtonReleased :
+			{
+				if(EventManager.mouseButton.button == sf::Mouse::Left)
+				{
+					releaseBox(EventManager.mouseButton.x, EventManager.mouseButton.y);
+				}
+
+				break;
+			}
+		}
+	}
+}
+
+
+//Launches bird if appropriate
+void releaseBox(int x, int y)
+{
+
+}
--- AngryBoxes/AngryBoxes/InputManager.h
+++ AngryBoxes/AngryBoxes/InputManager.h
@@ -1,5 +1,31 @@
 #ifndef InputManager_h__
 #define InputManager_h__
 
+#include <SFML/Window.hpp>
+
+
+class InputManager
+{
+
+public:
+
+	void init(sf::Window*);
+	void getInput();
+
+protected:
+
+	void releaseBox(int, int);
+
+	//instance of event class which stores event
+	sf::Event EventManager; 
+	sf::Window* windowPointer;
+	
+	int currentXPos, currentYPos;
+	int xDragStart, yDragStart;
+
+
+
+};
+
 
 #endif // InputManager_h__
