086fc1f2921acee0087a2a984296bf299d45ad9e
unknown <1303168@MC10329.uad.ac.uk>
2014-10-22 16:17:05 +0100

    Improved debug drawing routines

--- AngryBoxes/AngryBoxes/GraphicsContext.cpp
+++ AngryBoxes/AngryBoxes/GraphicsContext.cpp
@@ -1,6 +1,8 @@
 #include "GraphicsContext.h"
 #include "Texture.h"
 
+#define M_PI 3.14159265358979323846
+
 GraphicsContext::GraphicsContext() : window_(sf::VideoMode(800, 600), "Angry Boxes")
 {
 	tempSprite_.setTexture(redTexture.GetTexture());
@@ -15,6 +17,27 @@ void GraphicsContext::DrawOrientedBoundingBox(const OrientedBoundingBox& box)
 	polygon.setOutlineColor(sf::Color::White);
 	polygon.setFillColor(sf::Color::Transparent);
 	polygon.setOutlineThickness(1);
+
+	for(int i = 0; i < 2; i++)
+	{
+		float len = i == 0 ? box.width : box.height;
+		sf::RectangleShape axis(sf::Vector2f(box.origin[i] * 100, 1));
+		axis.setPosition(sf::Vector2f(box.center.x , box.center.y));
+		float rotation = atan2(box.axis[i].y, box.axis[i].x);
+		axis.setRotation(rotation * 180 / M_PI);
+
+		window_.draw(axis);
+		if( box.overlapInfo[i].overlaps )
+		{
+			sf::RectangleShape correction(sf::Vector2f(box.overlapInfo[i].max - box.overlapInfo[i].min * 100, 1));
+			correction.setPosition(sf::Vector2f(box.corner[0].x, box.corner[0].y));
+			correction.setRotation(rotation * 180 / M_PI);
+			correction.setFillColor(sf::Color::Blue);
+
+			window_.draw(correction);
+		}
+	}
+
 	window_.draw(polygon);
 }
 
