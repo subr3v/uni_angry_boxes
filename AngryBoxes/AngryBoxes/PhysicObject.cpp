679ba7622f048eebee8fcf47b225c1e245cb1905
unknown <1303168@MC10329.uad.ac.uk>
2014-10-22 15:03:43 +0100

    Added more properties to the physics object class.

--- AngryBoxes/AngryBoxes/PhysicObject.cpp
+++ AngryBoxes/AngryBoxes/PhysicObject.cpp
@@ -3,6 +3,6 @@
 PhysicsObject::PhysicsObject() { }
 
 PhysicsObject::PhysicsObject(float mass, const Vector2& position, const OrientedBoundingBox& shape) : m_position(position), m_velocity(Vector2(0,0)), m_mass(mass),
-							m_angularVelocity(0) , m_shape(shape) {
+							m_angularVelocity(0) , m_shape(shape), m_rotation(0), m_immovable(false) {
 
 }
--- AngryBoxes/AngryBoxes/PhysicObject.h
+++ AngryBoxes/AngryBoxes/PhysicObject.h
@@ -13,13 +13,17 @@ public:
 	inline const Vector2& velocity() const { return m_velocity; }
 	inline float angularVelocity() const { return m_angularVelocity; }
 	inline float mass() const { return m_mass; }
+	inline float rotation() const { return m_rotation; }
+	inline bool immovable() const { return m_immovable; }
 	inline const OrientedBoundingBox shape() { return m_shape; }
 
 	inline void position(const Vector2& position) { m_position = position; }
 	inline void velocity(const Vector2& velocity) { m_velocity = velocity; }
 	inline void angularVelocity(float angularVelocity) { m_angularVelocity = angularVelocity; }
 	inline void mass(float mass) { m_mass = mass; }
+	inline void rotation(float rotation) { m_rotation = rotation; }
 	inline void shape(const OrientedBoundingBox& shape) { m_shape = shape; }
+	inline void immovable(bool immovable) { m_immovable = immovable; }
 
 private:
 	friend class PhysicsSimulation;
@@ -29,6 +33,8 @@ private:
 	Vector2 m_position, m_velocity;
 	float m_angularVelocity;
 	float m_mass;
+	float m_rotation;
+	bool m_immovable;
 	OrientedBoundingBox m_shape;
 };
 
