31b4f29aedda3a7b29aa101f3d937ab95df21ada
Stefano Musumeci <subr3v@gmail.com>
2014-10-23 13:58:59 +0100

    fixup

--- AngryBoxes/AngryBoxes/main.cpp
+++ AngryBoxes/AngryBoxes/main.cpp
@@ -3,40 +3,45 @@
 #include "OrientedBoundingBox.h"
 #include "PhysicsSimulation.h"
 #include "PhysicObject.h"
+#include "InputManager.h"
 
 #define M_PI 3.14159265358979323846
 
 int main()
 {
+
+	InputManager input;
     // create the window
     //sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
 	GraphicsContext graphicsContext;
-	
+	input.init(graphicsContext.getWindow());
 	PhysicsSimulation simulation;
 	
-	simulation.gravity(Vector2(0, 9.8f));
+	simulation.gravity(Vector2(0, 2.8f));
 
-	PhysicsObject* obj = simulation.createObject(50, Vector2(150, 150), OrientedBoundingBox(Vector2(0, 0), 50, 50, 0));
-	PhysicsObject* obj1 = simulation.createObject(50, Vector2(180, 150), OrientedBoundingBox(Vector2(0, 0), 50, 50, 0));
-	PhysicsObject* ground = simulation.createObject(100, Vector2(400, 600 - 120), OrientedBoundingBox(Vector2(0, 0), 600, 120, 0));
-	ground->immovable(true);
-	obj1->immovable(true);
-	obj1->rotation(30.0f / 180.0f * M_PI);
+	PhysicsObject* obj = simulation.createObject(1, Vector2(190, 300), OrientedBoundingBox(Vector2(0, 0), 10, 50, 0));
+	//PhysicsObject* obj1 = simulation.createObject(0, Vector2(100, 200), OrientedBoundingBox(Vector2(0, 0), 400, 120, 20));
+	PhysicsObject* obj2 = simulation.createObject(1, Vector2(260, 20), OrientedBoundingBox(Vector2(0, 0), 10, 50, 20));
+	PhysicsObject* obj4 = simulation.createObject(1, Vector2(260, 320), OrientedBoundingBox(Vector2(0, 0), 10, 50, 20));
+	PhysicsObject* obj3 = simulation.createObject(1, Vector2(200, 20), OrientedBoundingBox(Vector2(0, 0), 10, 50, 20));
+	PhysicsObject* ground = simulation.createObject(0, Vector2(400, 600 - 120), OrientedBoundingBox(Vector2(0, 0), 600, 120, 0));
 
-	obj->angularVelocity(90.0f / 180.0f * M_PI);
+	obj->force = Vector2(150, -100);
+	//obj->rotation = -45;
+	//obj1->rotation = 45;
 
 	while (true)
 	{
 		graphicsContext.Update();
 		graphicsContext.Clear();
 
-		simulation.step(1.0f / 60.0f / 20.0f);
-		
 		const std::vector<PhysicsObject*>& objs = simulation.getObjects();
 		for(int i = 0; i < objs.size(); i++)
 		{
-			graphicsContext.DrawOrientedBoundingBox(objs[i]->shape());
+			graphicsContext.DrawOrientedBoundingBox(objs[i]->shape);
 		}
+
+		simulation.step(1.0f / 60.0f);
 
 		graphicsContext.SwapBuffers();
 	}
